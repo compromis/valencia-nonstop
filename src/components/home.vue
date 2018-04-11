36239358d4716ac5896c3caf31d9b2f85d5c32c7
Guillem <guillem@disedit.com>
2018-04-11 17:00:25 +0200

    Add basic KML support

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,7 @@
 <template>
 	<div>dsd
 		<gmap-map
+			ref="map"
 		  :center="{lat: 39.4751256, lng:-0.3831809}"
 		  :zoom="14"
 			:options="mapStyle"
@@ -21,8 +22,24 @@ export default {
 
 	data() {
 		return {
+			map: null,
+			mapLoaded: false,
 			mapStyle: { styles: mapStyle }
 		}
+	},
+
+	mounted() {
+		this.$refs.map.$mapCreated.then(() => {
+			this.mapLoaded = true;
+			this.$refs.map.$deferredReadyPromise.then(() => {
+				const options = {
+					url: "http://compromis.net/Test.kmz"
+				};
+
+				this.kml = new google.maps.KmlLayer(options);
+				this.kml.setMap(this.$refs.map.$mapObject);
+			});
+		});
 	}
 };
 </script>
