e860a573f5b3b9c4922b8d9cbe7f09b0a2f29af8
Guillem <guillem@disedit.com>
2018-05-09 15:16:11 +0200

    Style loading placeholder

--- src/components/partials/page-loading.vue
+++ src/components/partials/page-loading.vue
@@ -1,6 +1,14 @@
 <template>
 	<div class="page-loading">
-		Loading...
+		<div class="heading">
+			<span class="filler"></span>
+		</div>
+		<div class="subheading">
+			<span class="filler"></span>
+			<span class="filler" style="width: 60%"></span>
+			<span class="filler" style="width: 80%"></span>
+			<span class="filler" style="width: 70%"></span>
+		</div>
 	</div>
 </template>
 
