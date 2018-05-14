806df074eeb347e7f0c66c4342c32dbd30f6ea5c
Guillem <guillem@disedit.com>
2018-05-14 10:37:58 +0200

    Refresh styles when in dev mode

--- functions.php
+++ functions.php
@@ -1,20 +1,19 @@
 <?php
 
 function rt_rest_theme_scripts() {
-
-	wp_enqueue_style( 'style', get_stylesheet_uri() );
-
 	$base_url  = esc_url_raw( home_url() );
 	$base_path = rtrim( parse_url( $base_url, PHP_URL_PATH ), '/' );
 
 	if ( defined( 'RT_VUE_DEV' ) && RT_VUE_DEV ) {
-
-		wp_enqueue_script( 'rest-theme-vue', 'http://localhost:8080/dist/build.js', array( 'jquery' ), '1.0.0', true );
-
+		wp_enqueue_style( 'style', get_stylesheet_uri(), null, time());
 	} else {
+		wp_enqueue_style( 'style', get_stylesheet_uri() );
+	}
 
+	if ( defined( 'RT_VUE_DEV' ) && RT_VUE_DEV ) {
+		wp_enqueue_script( 'rest-theme-vue', 'http://localhost:8080/dist/build.js', array( 'jquery' ), '1.0.0', true );
+	} else {
 		wp_enqueue_script( 'rest-theme-vue', get_template_directory_uri() . '/dist/build.js', array( 'jquery' ), '1.0.0', true );
-
 	}
 
 	wp_localize_script( 'rest-theme-vue', 'rtwp', array(
@@ -29,14 +28,12 @@ function rt_rest_theme_scripts() {
 add_action( 'wp_enqueue_scripts', 'rt_rest_theme_scripts' );
 
 if ( function_exists( 'register_nav_menus' ) ) {
-
 	register_nav_menus(
 		array(
 		'primary-menu' => __( 'Primary Menu' ),
 		'secondary-menu' => __( 'Secondary Menu' ),
 		)
 	);
-
 }
 
 add_filter( 'excerpt_more', '__return_false' );
@@ -44,29 +41,19 @@ add_filter( 'excerpt_more', '__return_false' );
 add_action( 'after_setup_theme', 'rt_theme_setup' );
 
 function rt_theme_setup() {
-
 	add_theme_support( 'post-thumbnails' );
-
 }
 
 function rt_custom_rewrite_rule() {
-
 	global $wp_rewrite;
-
 	$wp_rewrite->front               = $wp_rewrite->root;
-
 	$wp_rewrite->set_permalink_structure( 'blog/%postname%/' );
-
 	$wp_rewrite->page_structure      = $wp_rewrite->root . 'page/%pagename%/';
-
 	$wp_rewrite->author_base         = 'author';
 	$wp_rewrite->author_structure    = '/' . $wp_rewrite->author_base . '/%author%';
-
 	$wp_rewrite->set_category_base( 'category' );
 	$wp_rewrite->set_tag_base( 'tag' );
-
 	$wp_rewrite->add_rule( '^blog$', 'index.php', 'top' );
-
 }
 add_action( 'init', 'rt_custom_rewrite_rule' );
 
@@ -74,20 +61,15 @@ add_action( 'init', 'rt_custom_rewrite_rule' );
 add_action( 'permalink_structure_changed', 'rt_forcee_perma_struct', 10, 2 );
 
 function rt_forcee_perma_struct( $old, $new ) {
-
 	update_option( 'permalink_structure', 'blog/%postname%' );
-
 }
 // Polyfill for wp_title()
 add_filter( 'wp_title','rt_vue_title', 10, 3 );
 
 function rt_vue_title( $title, $sep, $seplocation ) {
-
 	if ( false !== strpos( $title, __( 'Page not found' ) ) ) {
-
 		$replacement = ucwords( str_replace( '/', ' ', $_SERVER['REQUEST_URI'] ) );
 		$title       = str_replace( __( 'Page not found' ), $replacement, $title );
-
 	}
 
 	return $title;
@@ -97,7 +79,6 @@ function rt_vue_title( $title, $sep, $seplocation ) {
 add_action( 'rest_api_init', 'rt_extend_rest_post_response' );
 
 function rt_extend_rest_post_response() {
-
 	// Add featured image
 	register_rest_field( 'post',
 		'featured_image_src', //NAME OF THE NEW FIELD TO BE ADDED - you can call this anything
@@ -125,21 +106,17 @@ function rt_extend_rest_post_response() {
 			'schema'          => null,
 			)
 	);
-
 }
 // Get featured image
 function get_image_src( $object, $field_name, $request ) {
-
 	$feat_img_array['full'] = wp_get_attachment_image_src( $object['featured_media'], 'full', false );
 	$feat_img_array['thumbnail'] = wp_get_attachment_image_src( $object['featured_media'], 'thumbnail', false );
 	$feat_img_array['srcset'] = wp_get_attachment_image_srcset( $object['featured_media'] );
 	$image = is_array( $feat_img_array ) ? $feat_img_array : 'false';
 	return $image;
-
 }
 
 function rt_get_cat_name( $object, $field_name, $request ) {
-
 	$cats = $object['categories'];
 	$res = [];
 	$ob = [];
@@ -156,11 +133,9 @@ function rt_get_cat_name( $object, $field_name, $request ) {
 		}
 	}
 	return $res;
-
 }
 
 function rt_get_tag_name( $object, $field_name, $request ) {
-
 	$tags = $object['tags'];
 	$res = [];
 	$ob = [];
--- index.php
+++ index.php
@@ -8,40 +8,28 @@
 	<?php wp_head(); ?>
 </head>
 <body  <?php body_class() ?> >
-
-
-	<div id="content" class="site-content" style="display:none">
-
+	<div id="app">
 	<?php
-
-	if ( have_posts() ) {
-
-		if ( is_home() && ! is_front_page() ) {
-			echo '<h1>' . single_post_title( '', false ) . '</h1>';
-		}
-
-		while ( have_posts() ) {
-
-			the_post();
-
-			if ( is_singular() ) {
-				the_title( '<h1>', '</h1>' );
-			} else {
-				the_title( '<h2><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
+		if ( have_posts() ) {
+			if ( is_home() && ! is_front_page() ) {
+				echo '<h1>' . single_post_title( '', false ) . '</h1>';
 			}
 
-			the_content();
+			while ( have_posts() ) {
+				the_post();
+
+				if ( is_singular() ) {
+					the_title( '<h1>', '</h1>' );
+				} else {
+					the_title( '<h2><a href="' . esc_url( get_permalink() ) . '">', '</a></h2>' );
+				}
+
+				the_content();
+			}
 		}
-	}
-
 	?>
-
 	</div>
 
-	<div id="app"></div>
-
 	<?php wp_footer(); ?>
-
 </body>
 </html>
-
