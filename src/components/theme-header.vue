35340db382ac1191cf71197318b16560751b599e
Guillem <guillem@disedit.com>
2018-05-19 13:29:52 +0200

    Secondary menu mobile optimizations

--- src/components/theme-header.vue
+++ src/components/theme-header.vue
@@ -14,10 +14,13 @@
 		</div>
 
 		<nav class="header-wrapper">
-			<div class="header">
+			<div class="header navbar-dark">
+				<button class="navbar-toggler hamburger" @click="toggleSecondaryMenu()" type="button" aria-controls="navbarNavAltMarkup" :aria-expanded="false" aria-label="Mostra el menú">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 				<router-link :to="{ name: 'frontpage' }" class="site-name">VLC, el canvi no para</router-link>
-				<div class="secondary-menu">
-					<ul>
+				<div :class="{ 'secondary-menu': true, 'secondary-menu-visible': secondary_menu_visible }">
+					<ul @click="toggleSecondaryMenu()">
 						<li v-for="item in secondary_menu" v-if="item.type != 'custom'" :class="item.classes" :key="item.url">
 							<router-link :to="{ name: 'page', params: { name: getUrlName( item.url ) }}"> {{ item.title }} </router-link>
 						</li>
@@ -57,7 +60,8 @@ export default {
 			primary_menu: [],
 			secondary_menu: [],
 			site_name: rtwp.site_name,
-			show_share_buttons: true
+			show_share_buttons: true,
+			secondary_menu_visible: false
 		};
 	},
 	methods: {
@@ -82,6 +86,10 @@ export default {
 
 		handleScroll() {
 			this.show_share_buttons = window.scrollY < 50;
+		},
+
+		toggleSecondaryMenu() {
+			this.secondary_menu_visible = !this.secondary_menu_visible;
 		}
 	},
 	created() {
