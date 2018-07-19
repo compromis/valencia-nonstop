c7a3bdca86bddb1c546969114e347433a011f79e
Guillem <guillem@disedit.com>
2018-07-19 13:04:50 +0200

    Add manifest assets plugin and disable postcss-calc

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,25 @@
-module.exports = {
-  chainWebpack: config => {
-    const vueRule = config.module.rule('vue')
+const ManifestPlugin = require('webpack-manifest-plugin')
 
-    // add replacement loader(s)
-    vueRule
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new ManifestPlugin()
+    ]
+  },
+  chainWebpack: config => {
+    // Add support for inline SVG
+    config.module
+      .rule('vue')
       .use('vue-svg-inline-loader')
       .loader('vue-svg-inline-loader')
+
+    // Temporarily disable postcss-calc optimizations due to
+    // an unresolved issue when combining cal() and env()
+    config
+      .plugin('optimize-css')
+      .tap(args => {
+        args[0].cssnanoOptions.preset = ['default', { calc: false }]
+        return args
+      })
   }
 }
